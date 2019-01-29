830439ffe14260f66f76b755cf3532d13cd6115f
Robert Harężlak <harezlak.robert7@gmail.com>
2019-01-29 22:34:01 +0100

    Add basic styles to the package

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included() {
+    const app = this._findHost();
+    app.import('vendor/ember-countries-phone.css');
+  }
 };
